5c682f097cddd1081202fb7f8e98a5dabc39d8fb
haojiewa <wanghaojie@139.com>
2014-09-23 16:30:50 +0800

    gulp update

--- public/js/view/received.list.view.js
+++ public/js/view/received.list.view.js
@@ -47,6 +47,10 @@ define(['control/event.center', 'model/mail.model', 'model/platform.model', 'uti
     var update_received = function (i_data) {
       g_Mail.get_received_list(i_data, function (err, mails) {
         var i;
+        var j;
+        var pjson;
+        var plist;
+        var pshow;
         elem.base_dom.html('');
         var platform_sent = [];
         var show_click_fun = function (mail) {
@@ -56,18 +60,19 @@ define(['control/event.center', 'model/mail.model', 'model/platform.model', 'uti
         };
         for (i = mails.length - 1; i >= 0; i -= 1) {
           platform_sent = platform_sent.concat(mails[i].mail_info.platforms);
-          var pjson = JSON.stringify(mails[i].mail_info.platforms);
-          var plist = pjson.substring(1, pjson.length-1).split(',');
-          var pshow = '';
-          for(var j=0; j<plist.length ; j++){
-            if(j>0){
+          pjson = JSON.stringify(mails[i].mail_info.platforms);
+          plist = pjson.substring(1, pjson.length - 1).split(',');
+          for (j = 0; j < plist.length; j += 1) {
+            if (j > 0) {
               pshow += '<br />';
+            } else {
+              pshow = '';
             }
-            pshow += plist[j].substring(1, plist[j].length-1);
+            pshow += plist[j].substring(1, plist[j].length - 1);
           }
           elem.base_dom.append(
             $('<li class="received_list_item list-group-item"></li>').append(
-              $('<span class="item_platform"></span>').html(pshow|| ''),
+              $('<span class="item_platform"></span>').html(pshow || ''),
               $('<span class="item_from"></span>').html(mails[i].mail_header.from.split("@")[0]),
               $('<span class="item_release"></span>').html(mails[i].mail_info.release || ''),
               $('<span class="item_domain"></span>').html(mails[i].mail_info.domain || ''),
